f7919429c06fcc0b2813c0f0ef3714e9834b2586
Orel <orel.natan@devsense.co.il>
2018-12-13 13:47:59 +0200

    handle ngif

--- src/app/Pages/Home/Home.page.ts
+++ src/app/Pages/Home/Home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Service } from '../../Services/Service.service';
+import * as _ from 'lodash'; // npm install --save lodash
 
 @Component({
   selector: 'home',
@@ -10,12 +11,35 @@ import { Service } from '../../Services/Service.service';
 
 export class Home {
 
+  professions = _.times(5, (index) => ({
+    id      : Math.floor(Math.random() * 100) + 999,
+    label   :'profession' + index,
+  }));
+
   show;
+
+  initialValues = {
+    fullName: 'Orel Natan',
+    email: '',
+    phone: '',
+    gender: null,
+    religion: null,
+    profession: this.professions[3].id,
+    terms: null,
+    policy: null,
+    date: new Date(),
+  }
 
   constructor(private service: Service){
       this.service.getSomting().subscribe((resp) => {
-        console.log(resp)
+        console.log(this.professions);
       })
+  }
+
+
+  handleSubmit(initialValues){
+
+    console.log('initialValues: ', initialValues);
   }
 
 }
